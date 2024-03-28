6bb4d3979bbe38cc2cd632f3a8dbd535798608de
Gal Cohen <gal.natavi@gmail.com>
2024-03-28 13:03:36 +0200

    server progress

--- Server/Server.py
+++ Server/Server.py
@@ -6,14 +6,6 @@ import threading
 import select
 
 
-
-def get_available_port():
-    temp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    temp_socket.bind(('', 0))
-    port = temp_socket.getsockname()[1]
-    temp_socket.close()
-    return port
-
 def create_message(server_name,port):
     magic_cookie = 0xabcddcba
     message_type = 0x2
@@ -21,85 +13,107 @@ def create_message(server_name,port):
     server_name = server_name.ljust(32, '\0')
     server_port = port
     message = magic_cookie.to_bytes(4, byteorder='big') + message_type.to_bytes(1, byteorder='big') + server_name.encode() + server_port.to_bytes(2, byteorder='big')
-    return message, server_port
+    return message
+
+
+def create_question_bank():
+    #return a dictionary of questions and answers true or false
+    return {"question1": True, "question2": False, "question3": True, "question4": False, "question5": True}
+
 
 class Server:
     server_name = "PizzaProject"
+
     state = 0  # 0 = waiting for client, 1 = game mode
     timeout = 10
-    player_socket = []
-    player_name = []
-    addr = []
-
+    question_index = 0
+    players = {} # map of player sockets to their names and scores
+    questions = create_question_bank()
     def __init__(self):
-        self.port = get_available_port()
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    def start(self):
-        self.socket.bind(('localhost', self.port))
-        self.socket.listen(3)
-        print("Server started, listening on IP address "
-              + str(socket.gethostbyname(socket.gethostname())) + ":" + str(self.port))
-        message = create_message(self.server_name, self.port)
-        self.start_broadcasting(message)
-        self.start_receiver(self, 0)
-        self.start_receiver(self, 1)
-        self.start_receiver(self, 2)
+        self.UDP_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        # self.UDP_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+        self.UDP_socket.bind(('', 0))
+
+        self.TCP_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.TCP_socket.bind(('', 0))
+        self.port = self.TCP_socket.getsockname()[1]
+
+        self.UDP_thread = None
+        self.TCP_thread = None
+    def main_loop(self):
+        #start UDP thread
+        self.UDP_server()
+        #start TCP thread
+        # self.TCP_server()
 
 
-    def close(self):
-        self.socket.close()
 
-    def sendOutMessage(self, message):
-        self.socket.send(message.encode())
-        print("Message sent: " + message)
+    def UDP_server(self):
 
-    def broadcast(self, message):
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # use UDP
-        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)  # enable broadcasting
+        message = create_message(self.server_name,self.port)
+        # self.UDP_thread = threading.Thread(target=self.send_UDP_message, args=(message,))
+        # self.UDP_thread.start()
+        # self.send_UDP_message(message)
+        self.UDP_socket.sendto(message, ('<broadcast>', 13117))
+
+    def send_UDP_message(self, message):
         while self.state == 0:
-            self.socket.sendto(message.encode(), ('<broadcast>', self.port))
-            print("Broadcast message sent: " + message)
-
-    def start_broadcasting(self, message):
-        broadcast_thread = threading.Thread(target=self.broadcast, args=(message,))
-        broadcast_thread.start()
-
-    def receive(self, rc_id, timeout=10):
-        client_ready = False
-        while not client_ready:
-            readable, writable, exceptional = select.select([self.socket], [], [], timeout)
-            if self.state == 0:
-                for s in readable:
-                    data, addr = s.recvfrom(1024)
-                    print("Received message: " + data.decode() + " from " + str(addr))
-                    self.player_name[rc_id] = data.decode()
-                    self.player_socket[rc_id] = s
-                    self.addr[rc_id] = addr
-                    client_ready = True
-
-    def start_receiver(self, rc_id, timeout=10):
-        receive_thread = threading.Thread(target=self.receive, args=(rc_id, timeout))
-        receive_thread.start()
-
-    def timer(self):
-        # start timer
-        num_of_players = 0
-        start_time = time.time()
-        while num_of_players < 3:
+            self.UDP_socket.sendto(message, ('<broadcast>', 13117))
             time.sleep(1)
-            if time.time() - start_time > self.timeout:
-                print("Game has begun.")
-                self.state = 1
+
+    def TCP_server(self):
+        self.TCP_socket.bind(('', self.port))
+        self.TCP_socket.listen(1)
+        self.TCP_socket.settimeout(1)
+        # accept connection from client
+        self.TCP_thread = threading.Thread(target=self.TCP_accept)
+        self.TCP_thread.start()
+
+    def TCP_accept(self):
+        last_accepted = None
+        while True:
+            if last_accepted is not None and time.time() - last_accepted > self.timeout:
                 break
-            if self.state == 0:
-                num_of_players = 0
-                for i in range(3):
-                    if self.player_socket[i] is not None:
-                        num_of_players += 1
+            #accept connection
+            try:
+                client_socket, client_address = self.TCP_socket.accept()
+            except:
+                continue
 
+            if client_socket:
+                last_accepted = time.time()
 
+            self.players[client_socket] = {"name": None, "score": 0}
 
-    def start_timer(self):
-        timer_thread = threading.Thread(target=self.timer)
-        timer_thread.start()
+            print("Connection from: ", client_address)
+            # start thread for client
+            client_thread = threading.Thread(target=self.TCP_client, args=(client_socket,))
+            client_thread.start()
+
+        # kill UDP thread
+        print("Starting game...")
+        self.state = 1
+
+    def TCP_client(self, client_socket):
+        # first receive is name
+        if self.players[client_socket]["name"] is None:
+            data = client_socket.recv(1024)
+            self.players[client_socket]["name"] = data.decode()
+            print("Client name: ", self.players[client_socket]["name"])
+
+        # send question
+        while self.state == 1:
+            question = list(self.questions.keys())[self.question_index]
+            client_socket.send(question.encode())
+            # receive answer
+            data = client_socket.recv(1024)
+            if data.decode() == str(self.questions[question]):
+                self.players[client_socket]["score"] += 1
+            self.question_index += 1
+            if self.question_index == len(self.questions):
+                self.state = 2
+                break
+
+server = Server()
+server.main_loop()
