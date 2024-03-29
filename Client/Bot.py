d788f018eede5fdb5c432f69116cef2571db50ba
Gal Cohen <gal.natavi@gmail.com>
2024-03-29 16:57:25 +0300

    HALAS

--- Client/Bot.py
+++ Client/Bot.py
@@ -6,34 +6,77 @@ class Bot(Client):
     def __init__(self, portListen):
         super().__init__(portListen)
         self.name = "BOT_" + self.name
+        self.send = False
+        self.condition = threading.Condition()
         
 
     
-    def TCP_client(self):
-        
-        # set the timeout to 20 seconds
-        self.TCP_Socket.settimeout(20)
-        
+    # def TCP_client(self):
+    #
+    #     # set the timeout to 20 seconds
+    #     self.TCP_Socket.settimeout(20)
+    #
+    #
+    #     while not self.done:
+    #         try:
+    #             data = self.TCP_Socket.recv(1024)
+    #             # check if socket is closed
+    #             if not data:
+    #                 raise ConnectionError
+    #
+    #             answer = random.choice(["Y", "N"])
+    #
+    #             self.TCP_Socket.send(answer.encode())
+    #             print("Sent: " + answer)
+    #
+    #         except ConnectionError:
+    #             self.done = True
+    #             self.TCP_Socket.close()
+    #             break
+    #
+    #         except timeout:
+    #             continue
+
+    def receive_message_from_server(self):
         while not self.done:
             try:
                 data = self.TCP_Socket.recv(1024)
                 # check if socket is closed
                 if not data:
                     raise ConnectionError
-                
-                answer = random.choice(["Y", "N"])
-                
-                time.sleep(random.random() * 4 + 1)
-                self.TCP_Socket.send(answer.encode())
-                print("Sent: " + answer)
-                
-            except ConnectionError:
-                self.done = True
-                self.TCP_Socket.close()
+                logging.info("Received: " + data.decode('utf-8'))
+                data = data.decode('utf-8').split('\x00')[0]
+                print("\n", data)
+                with self.condition:
+                    self.condition.notify_all()
+                    # print("Notified")
+            # except timeout error
+            except timeout:
+                print("Connection timed out")
+                continue
+            except ConnectionError or ConnectionResetError:
+                print("Connection closed")
+                if not self.done:
+                    self.done = True
+                    with self.condition:
+                        self.condition.notify_all()
                 break
 
-            except timeout:
-                continue
+    def send_data_to_server(self):
+        while not self.done:
+            try:
+                # input timeout for 5 seconds
+                message = random.choice(["Y", "N"])
+                self.TCP_Socket.send(message.encode())
+                logging.info("Sent: " + message)
+                print("Sent: " + message)
+                with self.condition:
+                    self.condition.wait()
+                    # print("Waited")
+            except ConnectionError:
+                print("Connection closed")
+                self.done = True
+                break
 
 
 if __name__ == "__main__":
